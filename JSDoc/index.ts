cc33c861a7b9a10d2e0a7a280e1b6bf173dbd7c0
cs0041 <pamzalove20@hotmail.com>
2022-12-12 00:30:29 +0700

    JSDoc

---
+++ JSDoc/index.ts
@@ -0,0 +1,11 @@
+/**
+ * function find sum 2 parameters
+ * 
+ * @param a parameter a
+ * @param b parameter b
+ * @returns  sum a + b
+ */
+
+function sum(a: number, b: number): number {
+    return a + b
+}
