049851e4b0601993d2d8d3e9c170004000664f73
cs0041 <pamzalove20@hotmail.com>
2022-12-12 14:53:56 +0700

    Namespaces

---
+++ Namespaces/index.ts
@@ -0,0 +1,9 @@
+import { A } from './a'
+import { B } from './b'
+import { num as numC } from './c'
+import { num as numD } from './d'
+
+console.log(A.num)
+console.log(B.num)
+console.log(numC)
+console.log(numD)
