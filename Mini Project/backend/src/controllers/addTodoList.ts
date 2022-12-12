7f15713d7ee16547fcd0f58bdfcfa5b3c12d338f
cs0041 <pamzalove20@hotmail.com>
2022-12-12 17:45:53 +0700

    backend

--- Mini Project/backend/src/controllers/addTodoList.ts
+++ Mini Project/backend/src/controllers/addTodoList.ts
@@ -4,9 +4,9 @@ import { addTodo, TodoItem } from '../models/todo'
 
 interface RequestBody extends Omit<TodoItem, 'id'> {
   // title:string
-  //description?: string
+  // description?: string
 }
-
+ 
 export default <RequestHandler<unknown,CommonResponseBody,RequestBody>>((req, res) => {
   const id = addTodo(req.body)
   res.status(201).send({
--- Mini Project/backend/src/index.ts
+++ Mini Project/backend/src/index.ts
@@ -1,23 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import getTodoList from './controllers/getTodoList'
+import addTodoList from './controllers/addTodoList'
+import cors from 'cors'
+// import proxy from 'express-http-proxy'
 
-export interface CommonResponseBody{
-    data?: any
+
+export interface CommonResponseBody {
+  data?: any
 }
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 
 app.get('/api/todo', getTodoList)
-app.post('/api/todo', getTodoList)
+app.post('/api/todo', addTodoList)
+// app.use('/', proxy('http://127.0.0.1:4000/'))
 
-
-const port = Number ( process.env.PORT || 3000 )
-
-app.listen(port, ()=>{
-    console.log(`Server listening on port http://localhost:${port}`)
+const port = Number(process.env.PORT || 3000)
+app.listen(port, () => {
+  console.log(`App listening http://localhost:${port}/`)
 })
